744f3d6f884dacb0bf8095c53a4a79605d62749c
joost booij <joostbooij@MacBook-Pro-van-joost.local>
2025-12-19 14:07:43 +0100

    added storage funcionality to patch

--- Src/Settings/oscillator.h
+++ Src/Settings/oscillator.h
@@ -3,10 +3,18 @@
 
 class Oscillator {
  public:
-   enum Type { SAW, TRIANGLE, SQUARE, SINE, NOISE };
+  enum Type { SAW, TRIANGLE, SQUARE, SINE, NOISE };
 
   void init() {
     //
+  }
+
+  Type selectedOsc1() {
+    return selectedOsc1_;
+  }
+
+  Type selectedOsc2() {
+    return selectedOsc2_;
   }
 
   bool fmEnable() {
@@ -29,9 +37,9 @@ class Oscillator {
     return selectedOsc1_;
   }
 
-int type1SwitchValue() {
-  return 0;
-}
+  int type1SwitchValue() {
+    return 0;
+  }
 
   Type type2() {
     return selectedOsc2_;
@@ -43,6 +51,40 @@ int type1SwitchValue() {
 
   float shape2() {
     return shape2_;
+  }
+
+  // Storage
+  void save(FileWriter& fileWriter) {
+    fileWriter.write(fmEnable_);
+    fileWriter.write(amEnable_);
+    fileWriter.write(muteOsc1_);
+    fileWriter.write(muteOsc2_);
+    fileWriter.write(shape1_);
+    fileWriter.write(shape2_);
+    fileWriter.write(selectedOsc1_);
+    fileWriter.write(selectedOsc2_);
+  }
+
+  void load(FileReader& fileReader) {
+    fileReader.read(fmEnable_);
+    fileReader.read(amEnable_);
+    fileReader.read(muteOsc1_);
+    fileReader.read(muteOsc2_);
+    fileReader.read(shape1_);
+    fileReader.read(shape2_);
+    fileReader.read(selectedOsc1_);
+    fileReader.read(selectedOsc2_);
+  }
+
+  void paste(Oscillator* oscillator) {
+    fmEnable_ = oscillator->fmEnable();
+    amEnable_ = oscillator->amEnable();
+    muteOsc1_ = oscillator->muteOsc1();
+    muteOsc2_ = oscillator->muteOsc2();
+    shape1_ = oscillator->shape1();
+    shape2_ = oscillator->shape2();
+    selectedOsc1_ = oscillator->selectedOsc1();
+    selectedOsc1_ = oscillator->selectedOsc2();
   }
 
  private:
--- Src/Settings/patch.h
+++ Src/Settings/patch.h
@@ -10,16 +10,15 @@
 
 class Patch {
  public:
- 
   void init() {
+    lfo(0).init();
+    lfo(1).init();
     midi().init();
     oscillator().init();
     filter().init();
     ampEnvelope().init();
     modEnvelope().init();
     modMatrix().init();
-    lfo(0).init();
-    lfo(1).init();
   }
 
   Midi& midi() {
@@ -48,6 +47,40 @@ class Patch {
 
   Lfo& lfo(int index) {
     return lfo_[index];
+  }
+
+  // Storage
+  void save(FileWriter& fileWriter) {
+    lfo_[0].save(fileWriter);
+    lfo_[1].save(fileWriter);
+    midi_.save(fileWriter);
+    oscillator_.save(fileWriter);
+    filter_.save(fileWriter);
+    ampEnvelope_.save(fileWriter);
+    modEnvelope_.save(fileWriter);
+    modMatrix_.save(fileWriter);
+  }
+
+  void load(FileReader& fileReader) {
+    lfo_[0].load(fileReader);
+    lfo_[1].load(fileReader);
+    midi_.load(fileReader);
+    oscillator_.load(fileReader);
+    filter_.load(fileReader);
+    ampEnvelope_.load(fileReader);
+    modEnvelope_.load(fileReader);
+    modMatrix_.load(fileReader);
+  }
+
+  void paste(Patch* patch) {
+    lfo_[0].paste(&patch->lfo(0));
+    lfo_[1].paste(&patch->lfo(1));
+    midi_.paste(&patch->midi());
+    oscillator_.paste(&patch->oscillator());
+    filter_.paste(&patch->filter());
+    ampEnvelope_.paste(&patch->ampEnvelope());
+    modEnvelope_.paste(&patch->modEnvelope());
+    modMatrix_.paste(&patch->modMatrix());
   }
 
  private:
