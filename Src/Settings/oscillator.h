a69bba5a73c17fa38c1cc56849c7a5a647ede7a5
joost booij <joostbooij@MacBook-Pro-van-joost.local>
2025-12-20 06:56:58 +0100

    implemented oscillator class

--- Src/Settings/oscillator.h
+++ Src/Settings/oscillator.h
@@ -1,56 +1,143 @@
 #if !defined(Oscillator_h)
 #define Oscillator_h
 
+#include "settingsText.h"
+
 class Oscillator {
  public:
-  enum Type { SAW, TRIANGLE, SQUARE, SINE, NOISE };
+  enum Type1 { SAW1, TRIANGLE1, SINE1, SQUARE1, NUM_TYPES1 };
+  enum Type2 { SAW2, TRIANGLE2, NOISE2, SQUARE2, NUM_TYPES2 };
 
   void init() {
     //
   }
 
-  Type selectedOsc1() {
-    return selectedOsc1_;
+  static const char* type1Text(Type1 type) {
+    switch (type) {
+      case SAW1:      return "SAW";
+      case TRIANGLE1: return "TRIANGLE";
+      case SINE1:     return "SINE";
+      case SQUARE1:   return "SQUARE";
+      default:
+        break;
+    }
+    return nullptr;
   }
 
-  Type selectedOsc2() {
-    return selectedOsc2_;
+  static const char* type2Text(Type2 type) {
+    switch (type) {
+      case SAW2:      return "SAW";
+      case TRIANGLE2: return "TRIANGLE";
+      case NOISE2:    return "NOISE";
+      case SQUARE2:   return "SQUARE";
+      default:
+        break;
+    }
+    return nullptr;
   }
 
+  // Type 1
+  Type1 type1() {
+    return type1_;
+  }
+
+  void setType1(int value) {
+    type1_ = Type1(SettingsUtils::clip(0, NUM_TYPES1 - 1, value));
+  }
+
+  const char *type1Text() {
+    return type1Text(type1());
+  }
+
+  // Type 2
+  Type2 type2() {
+    return type2_;
+  }
+
+  void setType2(int value) {
+    type2_ = Type2(SettingsUtils::clip(0, NUM_TYPES2 - 1, value));
+  }
+
+  const char *type2Text() {
+    return type2Text(type2());
+  }
+
+  // FM enable
   bool fmEnable() {
     return fmEnable_;
   }
 
+  void setFmEnable(bool value) {
+    fmEnable_ = value;
+  }
+
+  const char *fmEnableText() {
+    return SettingsText::boolToOnOff(fmEnable());
+  }
+
+  // AM enable
   bool amEnable() {
     return amEnable_;
   }
 
+  void setAmEnable(bool value) {
+    amEnable_ = value;
+  }
+
+  const char *amEnableText() {
+    return SettingsText::boolToOnOff(amEnable());
+  }
+
+  // Mute Osc 1
   bool muteOsc1() {
     return muteOsc1_;
   }
 
+  void setMuteOsc1(bool value) {
+    muteOsc1_ = value;
+  }
+
+  const char *muteOsc1Text() {
+    return SettingsText::boolToOnOff(muteOsc1());
+  }
+
+   // Mute Osc 2
   bool muteOsc2() {
     return muteOsc2_;
   }
 
-  Type type1() {
-    return selectedOsc1_;
+  void setMuteOsc2(bool value) {
+    muteOsc2_ = value;
   }
 
-  int type1SwitchValue() {
-    return 0;
+  const char *muteOsc2Text() {
+    return SettingsText::boolToOnOff(muteOsc2());
   }
 
-  Type type2() {
-    return selectedOsc2_;
-  }
-
+  // Shape 1
   float shape1() {
     return shape1_;
   }
 
+  void setShape1(float value) {
+    shape1_ = SettingsUtils::clipFloat(value);
+  }
+
+  const char *shape1Text() {
+    return SettingsText::floatToText(shape1());
+  }
+
+  // Shape 2
   float shape2() {
     return shape2_;
+  }
+
+  void setShape2(float value) {
+    shape2_ = SettingsUtils::clipFloat(value);
+  }
+
+  const char *shape2Text() {
+    return SettingsText::floatToText(shape2());
   }
 
   // Storage
@@ -61,8 +148,8 @@ class Oscillator {
     fileWriter.write(muteOsc2_);
     fileWriter.write(shape1_);
     fileWriter.write(shape2_);
-    fileWriter.write(selectedOsc1_);
-    fileWriter.write(selectedOsc2_);
+    fileWriter.write(type1_);
+    fileWriter.write(type2_);
   }
 
   void load(FileReader& fileReader) {
@@ -72,8 +159,8 @@ class Oscillator {
     fileReader.read(muteOsc2_);
     fileReader.read(shape1_);
     fileReader.read(shape2_);
-    fileReader.read(selectedOsc1_);
-    fileReader.read(selectedOsc2_);
+    fileReader.read(type1_);
+    fileReader.read(type2_);
   }
 
   void paste(Oscillator* oscillator) {
@@ -83,8 +170,8 @@ class Oscillator {
     muteOsc2_ = oscillator->muteOsc2();
     shape1_ = oscillator->shape1();
     shape2_ = oscillator->shape2();
-    selectedOsc1_ = oscillator->selectedOsc1();
-    selectedOsc1_ = oscillator->selectedOsc2();
+    type1_ = oscillator->type1();
+    type2_ = oscillator->type2();
   }
 
  private:
@@ -94,8 +181,8 @@ class Oscillator {
   bool muteOsc2_;
   float shape1_;
   float shape2_;
-  Type selectedOsc1_;
-  Type selectedOsc2_;
+  Type1 type1_;
+  Type2 type2_;
 };
 
 #endif  // Oscilator_h
