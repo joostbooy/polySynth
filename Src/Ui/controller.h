694e0f0ae7a8f22b230513c9d4fd239daa271c02
joost booij <joostbooij@MacBook-Pro-van-joost.local>
2026-02-05 10:08:39 +0100

    added button names

--- Src/Ui/controller.h
+++ Src/Ui/controller.h
@@ -11,37 +11,53 @@ class Controller {
 public:
 
 	enum Id {
-		FUNCTION_BUTTON_A			= SERIAL_ID(3, 7),
-		FUNCTION_BUTTON_B			= SERIAL_ID(4, 7),
-		FUNCTION_BUTTON_C			= SERIAL_ID(5, 7),
-		FUNCTION_BUTTON_D			= SERIAL_ID(6, 7),
-		FUNCTION_ENC_A				= SERIAL_ID(3, 2),
-		FUNCTION_ENC_B				= SERIAL_ID(4, 2),
-		FUNCTION_ENC_C				= SERIAL_ID(5, 2),
-		FUNCTION_ENC_D				= SERIAL_ID(6, 2),
-		FUNCTION_ENC_PUSH_A			= SERIAL_ID(3, 4),
-		FUNCTION_ENC_PUSH_B			= SERIAL_ID(4, 4),
-		FUNCTION_ENC_PUSH_C			= SERIAL_ID(5, 4),
-		FUNCTION_ENC_PUSH_D			= SERIAL_ID(6, 4),
-		UP_BUTTON					= SERIAL_ID(1, 6),
-		DOWN_BUTTON					= SERIAL_ID(1, 7),
-		LEFT_BUTTON					= SERIAL_ID(0, 7),
-		RIGHT_BUTTON				= SERIAL_ID(2, 7),
-		SHIFT_BUTTON				= SERIAL_ID(0, 6),
-		COPY_BUTTON					= SERIAL_ID(0, 5),
-		PASTE_BUTTON				= SERIAL_ID(1, 5),
-		CLEAR_BUTTON				= SERIAL_ID(2, 5),
-		MENU_BUTTON					= SERIAL_ID(2, 6),
-		SAMPLE_CHAPTER_BUTTON		= SERIAL_ID(1, 0),
-		INSTRUMENT_CHAPTER_BUTTON	= SERIAL_ID(0, 0),
-		MODULATION_CHAPTER_BUTTON	= SERIAL_ID(1, 1),
-		SYSTEM_CHAPTER_BUTTON		= SERIAL_ID(2, 0),
-		MIDI_CHAPTER_BUTTON			= SERIAL_ID(0, 1),
-		STORAGE_CHAPTER_BUTTON		= SERIAL_ID(2, 1),
+		TUNE_1_OCT_UP			= SERIAL_ID(0, 0),
+		TUNE_1_OCT_DOWN			= SERIAL_ID(0, 1),
+		TUNE_2_OCT_UP			= SERIAL_ID(0, 2),
+		TUNE_2_OCT_DOWN			= SERIAL_ID(0, 3),
+		LFO_1_TYPE				= SERIAL_ID(0, 4),
+		LFO_2_TYPE				= SERIAL_ID(0, 5),
+		TUNE_1_VCO_MOD			= SERIAL_ID(1, 0),
+		TUNE_1_MOD_TYPE			= SERIAL_ID(1, 1),
+		VCO_2_SYNC				= SERIAL_ID(1, 2),
+		TUNE_2_MOD_TYPE			= SERIAL_ID(1, 3),
+		SHAPE_1_MOD_TYPE		= SERIAL_ID(2, 0),
+		VCO_1_OSC_TYPE			= SERIAL_ID(2, 1),
+		SHAPE_2_MOD_TYPE		= SERIAL_ID(2, 2),
+		VCO_2_OSC_TYPE			= SERIAL_ID(2, 3),
+		VCO_MOD_SOURCE			= SERIAL_ID(2, 4),
+		SLIDE_VCO_SELECT		= SERIAL_ID(2, 5),
+		CUTOFF_1_MOD_TYPE		= SERIAL_ID(3, 0),
+		VCO_1_MUTE				= SERIAL_ID(3, 1),
+		CUTOFF_2_MOD_TYPE		= SERIAL_ID(3, 2),
+		VCO_2_MUTE				= SERIAL_ID(3, 3),
+		VCO_MOD_DEPTH_MOD_TYPE	= SERIAL_ID(3, 4),
+		VOLUME_MOD_TYPE			= SERIAL_ID(3, 5),
+		VCF_1_TYPE				= SERIAL_ID(4, 0),
+		CUTOFF_1_VCO_MOD		= SERIAL_ID(4, 1),
+		VCF_2_ROUTING			= SERIAL_ID(4, 2),
+		CUTOFF_2_VCO_MOD		= SERIAL_ID(4, 3),
+		VOLUME_VCO_MOD			= SERIAL_ID(4, 5),
+		DISPLAY_B				= SERIAL_ID(5, 0),
+		DISPLAY_A				= SERIAL_ID(5, 1),
+		PATCH_PAGE				= SERIAL_ID(5, 2),
+		ENVELOPE_PAGE			= SERIAL_ID(5, 3),
+		VCO_PAGE				= SERIAL_ID(5, 4),
+		PAN_MOD_TYPE			= SERIAL_ID(5, 5),
+		DISPLAY_D				= SERIAL_ID(6, 0),
+		DISPLAY_C				= SERIAL_ID(6, 1),
+		LFO_PAGE				= SERIAL_ID(6, 2),
+		MATRIX_PAGE				= SERIAL_ID(6, 3),
+		SYSTEM_PAGE				= SERIAL_ID(6, 4),
+		SHIFT					= SERIAL_ID(6, 5),
+		ENC_X					= SERIAL_ID(7, 0),
+		AMP_PAGE				= SERIAL_ID(7, 2),
+		VCF_PAGE				= SERIAL_ID(7, 3),
+		ENC_Y					= SERIAL_ID(7, 5),
 	};
 
 	static void init() {
-		for (int i = 0; i < (8 * 8) / 32; ++i) {
+		for (int i = 0; i < 2; ++i) {
 			pressed_[i] = 0;
 		}
 	}
@@ -64,55 +80,28 @@ public:
 	static int8_t buttonToFunction(uint8_t id) {
 		switch (id)
 		{
-		case FUNCTION_BUTTON_A:		return 0;
-		case FUNCTION_BUTTON_B:		return 1;
-		case FUNCTION_BUTTON_C:		return 2;
-		case FUNCTION_BUTTON_D:		return 3;
-		default:					return -1;
+		case DISPLAY_A:		return 0;
+		case DISPLAY_B:		return 1;
+		case DISPLAY_C:		return 2;
+		case DISPLAY_D:		return 3;
+		default:			return -1;
 			break;
 		}
 	}
 
-	static int8_t buttonToChapter(uint8_t id) {
-		switch (id)
-		{
-		case SAMPLE_CHAPTER_BUTTON:		return Chapter::SAMPLE;
-		case INSTRUMENT_CHAPTER_BUTTON:	return Chapter::INSTRUMENT;
-		case MODULATION_CHAPTER_BUTTON:	return Chapter::MODULATION;
-		case SYSTEM_CHAPTER_BUTTON:		return Chapter::SYSTEM;
-		case MIDI_CHAPTER_BUTTON:		return Chapter::MIDI;
-		case STORAGE_CHAPTER_BUTTON:	return Chapter::STORAGE;
-		default:						return -1;
-			break;
-		}
-	}
-
-	static int8_t encoderToFunction(uint8_t id) {
-		switch (id)
-		{
-		case FUNCTION_ENC_A:	return 0;
-		case FUNCTION_ENC_B:	return 1;
-		case FUNCTION_ENC_C:	return 2;
-		case FUNCTION_ENC_D:	return 3;
-		default:				return -1;
-			break;
-		}
-	}
-
-	static bool encoderIsPressed(uint8_t encoder_id) {
-		switch (encoder_id)
-		{
-		case FUNCTION_ENC_A:	return isPressed(FUNCTION_ENC_PUSH_A);
-		case FUNCTION_ENC_B:	return isPressed(FUNCTION_ENC_PUSH_B);
-		case FUNCTION_ENC_C:	return isPressed(FUNCTION_ENC_PUSH_C);
-		case FUNCTION_ENC_D:	return isPressed(FUNCTION_ENC_PUSH_D);
-		default:				return false;
-			break;
-		}
-	}
+        /*
+                static int8_t buttonToPage(uint8_t id) {
+                        switch (id)
+                        {
+                        case LFO_PAGE:					return Pages::LFO_PAGE;
+                        default:						return -1;
+                                break;
+                        }
+                }
+        */
 
 private:
-	static inline uint32_t pressed_[(8 * 8) / 32];
+	static inline uint32_t pressed_[2];
 };
 
 #endif
