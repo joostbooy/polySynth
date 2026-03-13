4ab6d8a41a98b0155d430e0d2d02221f95940097
Joost Booij <joostbooy@gmail.com>
2026-03-13 14:49:43 +0100

    refactored lfo engine

--- Src/Engine/lfoEngine.h
+++ Src/Engine/lfoEngine.h
@@ -8,24 +8,18 @@
 class LfoEngine {
  public:
   enum Stage {
-    FALLING = 0,
-    RISING = 1,
+    RISING,
+    FALLING,
   };
 
   void init(Lfo* lfo) {
     lfo_ = lfo;
     reset();
-
-    if (stage_ == RISING) {
-      lastValue_ = lfo_->min();
-    } else {
-      lastValue_ = lfo_->max();
-    }
   }
 
   void reset() {
     phase_ = 0.f;
-    setStage(nextPhase() < lfo_->skew() ? RISING : FALLING, true);
+    randomMax_ = Rng::reciprocal(lfo_->min(), lfo_->max());
   }
 
   void retrigger(int playOrder) {
@@ -39,21 +33,17 @@ class LfoEngine {
     return phase_;
   }
 
-  float value() {
-    return value_;
-  }
-
   float next() {
     float skewPhase;
     float skewAmount = EngineUtils::spread(lfo_->skew(), lfo_->skewSpread(), playOrder_);
     float phase = nextPhase();
 
     if (phase < skewAmount) {
-      setStage(RISING);
+      stage_ = RISING;
       skewPhase = phase * (1.0f / skewAmount);
     } else {
-      setStage(FALLING);
-      skewPhase = (phase - skewAmount) * (1.0f / (1.0f - skewAmount));
+      stage_ = FALLING;
+      skewPhase = 1.f - (phase - skewAmount) * (1.0f / (1.0f - skewAmount));
     }
 
     float x = 0.f;
@@ -63,10 +53,18 @@ class LfoEngine {
         x = skewPhase;
         break;
       case Lfo::EXP_LOG:
-        x = LookupTablesUtils::readInterpolated(lut_exp, skewPhase);
+        if (stage_ == RISING) {
+          x = LookupTablesUtils::readInterpolated(lut_exp, skewPhase);
+        } else {
+          x = LookupTablesUtils::readInterpolated(lut_inv_exp, skewPhase);
+        }
         break;
       case Lfo::LOG_EXP:
-        x = LookupTablesUtils::readInterpolated(lut_inv_exp, skewPhase);
+        if (stage_ == RISING) {
+          x = LookupTablesUtils::readInterpolated(lut_inv_exp, skewPhase);
+        } else {
+          x = LookupTablesUtils::readInterpolated(lut_exp, skewPhase);
+        }
         break;
       case Lfo::SQUARE:
         x = phase_ < skewAmount ? 1.f : 0.f;
@@ -75,32 +73,17 @@ class LfoEngine {
         break;
     }
 
-    value_ = Dsp::cross_fade(lastValue_, targetValue_, x);
-    return value_;
+    float min = lfo_->min();
+    float max = lfo_->randomise() ? randomMax_ : lfo_->max();
+    return min + (max - min) * x;
   }
 
  private:
   Lfo* lfo_;
-  Stage stage_ = RISING;
   float phase_ = 0.f;
-  float value_ = 0.f;
-  float lastValue_ = 0.f;
-  float targetValue_ = 1.f;
+  float randomMax_ = 1.f;
   int playOrder_ = 0;
-
-  inline void setStage(Stage stage, bool force = false) {
-    if (stage_ != stage || force == true) {
-      stage_ = stage;
-
-      lastValue_ = value_;
-
-      if (lfo_->randomise()) {
-        targetValue_ = Rng::reciprocal(lfo_->min(), lfo_->max());
-      } else {
-        targetValue_ = (stage_ == RISING) ? lfo_->max() : lfo_->min();
-      }
-    }
-  }
+  Stage stage_;
 
   float readInc() {
     if (lfo_->clockSync()) {
@@ -122,7 +105,11 @@ class LfoEngine {
 
     phase_ += readInc();
     if (phase_ >= 1.f) {
-      phase_ = lfo_->oneShot() ? 1.f : 0.f;
+      if (lfo_->oneShot()) {
+        phase_ = 1.f;
+      } else {
+        reset();
+      }
     }
 
     return offsetPhase;
