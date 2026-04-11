dcdaaad0cbe530498ada1ab5c96632f6a51bf005
Joost Booij <joostbooy@gmail.com>
2026-04-11 11:39:38 +0200

    changed cs pin

--- Drivers/eeprom.cpp
+++ Drivers/eeprom.cpp
@@ -4,9 +4,9 @@ void Eeprom::init() {
     GPIO_InitTypeDef GPIO_InitStruct = {0};
 	
 	/**GPIO Configuration
-	PA15     ------> CS
+	PC10     ------> CS
 	*/
-	GPIO_InitStruct.Pin = GPIO_PIN_15;
+	GPIO_InitStruct.Pin = GPIO_PIN_10;
 	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
 	GPIO_InitStruct.Pull = GPIO_NOPULL;
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
