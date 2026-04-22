3cea2f9b2dc5d9d7a57e9d7551dbd87fc612a9c3
Joost Booij <joostbooy@gmail.com>
2026-04-22 10:01:04 +0200

    cleanup

--- Src/Ui/Pages/ampPage.h
+++ Src/Ui/Pages/ampPage.h
@@ -1,88 +1,88 @@
 #ifndef AmpPage_h
 #define AmpPage_h
 
-#include "listPage.h"
 #include "amp.h"
 #include "ampList.h"
+#include "listPage.h"
 
 namespace AmpPage {
 
-	using TopPage::settings_;
-	using TopPage::engine_;
-	using TopPage::ui_;
+  using TopPage::engine_;
+  using TopPage::settings_;
+  using TopPage::ui_;
 
-	bool pasteable_;
-	Amp amp_;
-	AmpList ampList_;
+  bool pasteable_;
+  Amp amp_;
+  AmpList ampList_;
 
-	void clear() {
-		settings_->amp().init();
-		ui_->resetAllPots();
-	}
+  void clear() {
+    settings_->amp().init();
+    ui_->resetAllPots();
+  }
 
-	void copy() {
-		amp_.paste(&settings_->amp());
-		pasteable_ = true;
-	}
+  void copy() {
+    amp_.paste(&settings_->amp());
+    pasteable_ = true;
+  }
 
-	bool paste() {
-		if (pasteable_) {
-			settings_->amp().paste(&amp_);
-			ui_->resetAllPots();
-			return true;
-		}
-		return false;
-	}
+  bool paste() {
+    if (pasteable_) {
+      settings_->amp().paste(&amp_);
+      ui_->resetAllPots();
+      return true;
+    }
+    return false;
+  }
 
-	void init() {
-		pasteable_ = false;
-		amp_.init();
-		ampList_.init(engine_, settings_, ui_);
-	}
+  void init() {
+    pasteable_ = false;
+    amp_.init();
+    ampList_.init(engine_, settings_, ui_);
+  }
 
-	void enter() {
-		ListPage::setList(&ampList_);
-		ListPage::setClearCallback(&clear);
-		ListPage::setCopyCallback(&copy);
-		ListPage::setPasteCallback(&paste);
-		ListPage::enter();
-	}
+  void enter() {
+    ListPage::setList(&ampList_);
+    ListPage::setClearCallback(&clear);
+    ListPage::setCopyCallback(&copy);
+    ListPage::setPasteCallback(&paste);
+    ListPage::enter();
+  }
 
-	void exit()  {
-		ListPage::exit();
-	}
+  void exit() {
+    ListPage::exit();
+  }
 
-	void onButton(int id, int state) {
-		ListPage::onButton(id, state);
-	}
+  void onButton(int id, int state) {
+    ListPage::onButton(id, state);
+  }
 
-	void onEncoder(int id, int state) {
-		ListPage::onEncoder(id, state);
-	}
+  void onEncoder(int id, int state) {
+    ListPage::onEncoder(id, state);
+  }
 
-	void refreshLeds() {
-		ListPage::refreshLeds();
-	}
+  void refreshLeds() {
+    ListPage::refreshLeds();
+  }
 
-	void draw() {
-		ListPage::draw();
-	}
+  void draw() {
+    ListPage::draw();
+  }
 
-	const size_t targetFps() {
-		return 1000 / 16;
-	}
+  const size_t targetFps() {
+    return 1000 / 16;
+  }
 
-	Pages::Page page = {
-		&init,
-		&enter,
-		&exit,
-		&draw,
-		&refreshLeds,
-		&onButton,
-		&onEncoder,
-		&targetFps
-	};
+  Pages::Page page = {
+      &init,
+      &enter,
+      &exit,
+      &draw,
+      &refreshLeds,
+      &onButton,
+      &onEncoder,
+      &targetFps,
+  };
 
-};
+};  // namespace AmpPage
 
 #endif
